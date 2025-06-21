0f49f55aa64de0122f3e66ec317711cc8701c088
Vaibhav Gupta <vaibhav101098@gmail.com>
2025-06-21 14:41:45 +0530

    promise all

--- src/pages/promise-all.js
+++ src/pages/promise-all.js
@@ -59,3 +59,9 @@ const index = () => {
 };
 
 export default index;
+
+/* Create a React web application that consumes the https://fakestoreapi.com/carts/2 API and displays a list of products in the cart. 
+ For each product in the cart, the application should make an API call to fetch the product details
+ using the ID returned in the cart API and display the product title, price, and image. 
+ Note: The https://fakestoreapi.com/products/:id: API returns details for a single product. 
+ You will need to make an API call for each product in the cart to fetch the product details. */
